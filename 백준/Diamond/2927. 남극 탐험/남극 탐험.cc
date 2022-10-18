362bd8818b644fecbef554a2be59b678606e966a
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2022-10-18 20:38:45 +0900

    [Diamond V] Title: 남극 탐험, Time: 380 ms, Memory: 36556 KB -BaekjoonHub

---
+++ 백준/Diamond/2927. 남극 탐험/남극 탐험.cc
@@ -0,0 +1,235 @@
+// 남극 탐험
+// 22.07.25
+// 엣지 or 정점간 경로의 유무(경로가 존재한다면 경로 상에 있는 값의 합)를 출력하는 문제
+// 미리 엣지를 입력받아서 분리 집합으로 사이클을 제거한 최종 트리를 구성해놓고 명령을 순서대로 처리
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+vector <vector<int>> node;
+vector <pair<int, int>> value;
+vector <int> segtree;
+vector <int> parent_HLD;
+vector <int> parent_SET;
+vector <int> sz;
+vector <int> depth;
+vector <int> section;
+vector <int> chain_number;
+vector <int> chain_index;
+vector <vector<int>> chain;
+vector <pair<string, pair<int, int>>> com;
+vector <bool> visit;
+int N, M, cnt;
+
+void set_parent()
+{
+	for (int i = 1; i <= N; i++)
+		parent_SET[i] = i;
+}
+
+int Find(int x)
+{
+	if (x == parent_SET[x]) 
+		return x;
+	return parent_SET[x] = Find(parent_SET[x]);
+}
+
+bool Sameset(int x, int y)
+{
+	int px = Find(x);
+	int py = Find(y);
+	if (px == py)
+		return true;
+	else
+		return false;
+}
+
+void Union(int x, int y)
+{
+	int px = Find(x);
+	int py = Find(y);
+	if (px != py)
+		parent_SET[px] = py;
+}
+
+void DFS(int n, int pn)
+{
+	visit[n] = true;
+	parent_HLD[n] = pn;
+	sz[n] = cnt++;
+	for (int x : node[n])
+		if (x != pn)
+			DFS(x, n);
+	sz[n] = cnt - sz[n];
+}
+
+void HLD(int n, int pn, int start_chain, int d)
+{
+	visit[n] = true;
+	depth[n] = d;
+	chain_number[n] = start_chain;
+	chain_index[n] = chain[start_chain].size();
+	chain[start_chain].push_back(n);
+
+	section[n] = cnt++;
+	value[cnt].second = value[n].first;
+	int heavy = - 1;
+	for (int x : node[n])
+		if (x != pn && (heavy == -1 || sz[x] > sz[heavy]))
+			heavy = x;
+	if (heavy != -1)
+		HLD(heavy, n, start_chain, d);
+
+	for (int x : node[n])
+		if (x != pn && x != heavy)
+			HLD(x, n, x, d + 1);
+}
+
+int LCA(int u, int v)
+{
+	while (chain_number[u] != chain_number[v])
+	{
+		if (depth[u] > depth[v])
+			u = parent_HLD[chain_number[u]];
+		else
+			v = parent_HLD[chain_number[v]];
+	}
+	return chain_index[u] > chain_index[v] ? v : u;
+}
+
+void set_segtree(int n, int start, int end)
+{
+	if (start == end)
+		segtree[n] = value[start + 1].second;
+	else
+	{
+		set_segtree(n * 2, start, (start + end) / 2);
+		set_segtree(n * 2 + 1, (start + end) / 2 + 1, end);
+		segtree[n] = segtree[n * 2] + segtree[n * 2 + 1];
+	}
+}
+
+void update(int n, int start, int end, int index, int change)
+{
+	if (start == end)
+		segtree[n] = change;
+	else
+	{
+		if (index <= (start + end) / 2)
+			update(n * 2, start, (start + end) / 2, index, change);
+		else
+			update(n * 2 + 1, (start + end) / 2 + 1, end, index, change);
+		segtree[n] = segtree[n * 2] + segtree[n * 2 + 1];
+	}
+}
+
+int get_sum(int n, int start, int end, int left, int right)
+{
+	if (right < start || left > end)
+		return 0;
+	if (left <= start && end <= right)
+		return segtree[n];
+	return get_sum(n * 2, start, (start + end) / 2, left, right) + get_sum(n * 2 + 1, (start + end) / 2 + 1, end, left, right);
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int u, v;
+	string ch;
+	cin >> N;
+	int h = (int)ceil(log2(N));
+	segtree = vector<int>((1 << (h + 1)));
+	node = vector<vector<int>>(N + 1);
+	value = vector<pair<int, int>>(N + 1);
+	parent_HLD = vector<int>(N + 1);
+	parent_SET = vector<int>(N + 1);
+	section = vector<int>(N + 1);
+	sz = vector<int>(N + 1);
+	depth = vector<int>(N + 1);
+	chain_number = vector<int>(N + 1);
+	chain_index = vector<int>(N + 1);
+	chain = vector<vector<int>>(N + 1);
+
+	for (int i = 1; i <= N; i++)
+		cin >> value[i].first;
+
+	set_parent();
+	cin >> M;
+	for (int i = 0; i < M; i++)
+	{
+		cin >> ch >> u >> v;
+		com.push_back(make_pair(ch, make_pair(u, v)));
+		if (ch == "bridge")
+		{
+			if (!Sameset(u, v))
+			{
+				node[u].push_back(v);
+				node[v].push_back(u);
+				Union(u, v);
+			}
+		}
+	}
+
+	cnt = 0;
+	visit = vector<bool>(N + 1, false);
+	for (int i = 1; i <= N; i++)
+		if (!visit[i])
+			DFS(i, 0);
+
+	cnt = 0;
+	visit = vector<bool>(N + 1, false);
+	for (int i = 1; i <= N; i++)
+		if (!visit[i])
+			HLD(i, 0, i, 0);
+
+	set_segtree(1, 0, N - 1);
+	set_parent();
+
+	for (int i = 0; i < M; i++)
+	{
+		ch = com[i].first;
+		u = com[i].second.first;
+		v = com[i].second.second;
+		if (ch == "excursion")
+		{
+			if (!Sameset(u, v))
+				cout << "impossible\n";
+			else
+			{
+				int t = LCA(u, v);
+				int sum = 0;
+				while (chain_number[t] != chain_number[u])
+				{
+					sum += get_sum(1, 0, N - 1, section[chain_number[u]], section[u]);
+					u = parent_HLD[chain_number[u]];
+				}
+				sum += get_sum(1, 0, N - 1, section[t] + 1, section[u]);
+				while (chain_number[t] != chain_number[v])
+				{
+					sum += get_sum(1, 0, N - 1, section[chain_number[v]], section[v]);
+					v = parent_HLD[chain_number[v]];
+				}
+				sum += get_sum(1, 0, N - 1, section[t] + 1, section[v]);
+				sum += get_sum(1, 0, N - 1, section[t], section[t]);
+				cout << sum << '\n';
+			}
+		}
+		else if (ch == "bridge")
+		{
+			if (!Sameset(u, v))
+			{
+				cout << "yes\n";
+				Union(u, v);
+			}
+			else
+				cout << "no\n";
+		}
+		else
+			update(1, 0, N - 1, section[u], v);
+	}
+
+	return 0;
+}
