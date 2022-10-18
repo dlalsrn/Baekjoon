74ef3711aabe652ad042756228996397a4fae4b6
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2022-10-18 21:18:31 +0900

    [Silver II] Title: 최대 힙, Time: 20 ms, Memory: 2764 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <stdlib.h>
+#include <math.h>
+#include <functional>
+using namespace std;
+
+int main(void)
+{
+	priority_queue <int> q;
+	int N;
+	scanf(" %d", &N);
+
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		scanf(" %d", &num);
+		if (num != 0)
+			q.push(num);
+		else
+		{
+			if (q.empty())
+			{
+				printf("0\n");
+			}
+			else
+			{
+				printf("%d\n", q.top());
+				q.pop();
+			}
+		}
+	}
+    return 0;
+}
