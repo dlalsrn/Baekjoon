235514af58aa0f6c2fb0be0eed5d5e93474975a5
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2022-10-18 20:44:28 +0900

    [Gold II] Title: 짝수번째 피보나치 수의 합, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/11443. 짝수번째 피보나치 수의 합/짝수번째 피보나치 수의 합.cc
@@ -0,0 +1,48 @@
+// 짝수번째 피보나치 수의 합
+#include <iostream>
+#include <map>
+#define MOD 1000000007
+typedef long long ll;
+using namespace std;
+
+ll fibo(ll n, map <ll, ll>* m)
+{
+	if (n == 0 || n == 1)
+		return (*m)[n];
+	else if ((*m)[n])
+		return (*m)[n];
+	else
+	{
+		if (n % 2 == 0)
+		{
+			ll t = fibo(n / 2, m) % MOD;
+			ll s = fibo(n / 2 - 1, m) % MOD;
+			(*m)[n / 2] = t;
+			(*m)[n / 2 - 1] = s;
+			return t * (2 * s + t) % MOD;
+		}
+		else
+		{
+			ll t = fibo(n / 2, m) % MOD;
+			ll s = fibo(n / 2 + 1, m) % MOD;
+			(*m)[n / 2] = t;
+			(*m)[n / 2 + 1] = s;
+			(*m)[n] = (t * t + s * s) % MOD;
+			return (t * t + s * s) % MOD;
+		}
+	}
+}
+
+int main(void)
+{
+	map <ll, ll> m;
+	m[0] = 0;
+	m[1] = 1;
+	ll a;
+	cin >> a;
+	if (a % 2 != 0)
+		cout << fibo(a, &m) - 1;
+	else
+		cout << fibo(a+1, &m) - 1;
+	return 0;
+}
