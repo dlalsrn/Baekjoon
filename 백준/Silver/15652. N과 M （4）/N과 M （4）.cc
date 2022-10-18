bf70dcd931f396d81fa2b402d4afb8dd542be77e
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2022-10-18 21:20:16 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,32 @@
+// N과 M (4)
+#include <iostream>
+#include <vector>
+using namespace std;
+int N, M;
+vector<int> v;
+
+void DFS()
+{
+	if (v.size() == M)
+	{
+		for (int i = 0; i < v.size(); i++)
+			cout << v[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	else
+	{
+		for (int i = (v.size() == 0 ? 1 : v.back()); i <= N; i++)
+		{
+			v.push_back(i);
+			DFS();
+			v.pop_back();
+		}
+	}
+}
+
+int main(void)
+{
+	cin >> N >> M;
+	DFS();
+}
