dc99e61598f215f413ea68ab30c838cfde901283
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2022-10-18 20:37:50 +0900

    [Gold V] Title: 용액, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int N, i, j, num, min_i, min_j, min = 2000000000;
+	vector <int> v;
+	cin >> N;
+	for (i = 0; i < N; i++)
+	{
+		cin >> num;
+		v.push_back(num);
+	}
+
+
+	i = 0;
+	min_i = 0;
+	j = v.size() - 1;
+	min_j = v.size() - 1;
+
+	while (i < j)
+	{
+		int temp = v[i] + v[j];
+		if (min > abs(temp))
+		{
+			min = abs(temp);
+			min_i = i;
+			min_j = j;
+		}
+		if (temp > 0)
+			j--;
+		else if (temp < 0)
+			i++;
+		else
+		{
+			min_i = i;
+			min_j = j;
+			break;
+		}
+	}
+	cout << v[min_i] << ' ' << v[min_j];
+	return 0;
+}
