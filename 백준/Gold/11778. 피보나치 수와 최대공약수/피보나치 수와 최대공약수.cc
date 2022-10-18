a28b95d5c068898084e9aac1130ec56188428ba8
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2022-10-18 20:44:48 +0900

    [Gold I] Title: 피보나치 수와 최대공약수, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Gold/11778. 피보나치 수와 최대공약수/피보나치 수와 최대공약수.cc
@@ -0,0 +1,53 @@
+// 피보나치 수의 제곱의 합
+#include <iostream>
+#include <map>
+#define MOD 1000000007
+typedef long long ll;
+using namespace std;
+
+ll fibo(ll n, map <ll, ll>* m)
+{
+	if (n == 0 || n == 1)
+		return (*m)[n];
+	else if ((*m)[n])
+		return (*m)[n];
+	else
+	{
+		if (n % 2 == 0)
+		{
+			ll t = fibo(n / 2, m) % MOD;
+			ll s = fibo(n / 2 - 1, m) % MOD;
+			(*m)[n / 2] = t;
+			(*m)[n / 2 - 1] = s;
+			(*m)[n] = t * (2 * s + t) % MOD;
+			return t * (2 * s + t) % MOD;
+		}
+		else
+		{
+			ll t = fibo(n / 2, m) % MOD;
+			ll s = fibo(n / 2 + 1, m) % MOD;
+			(*m)[n / 2] = t;
+			(*m)[n / 2 + 1] = s;
+			(*m)[n] = (t * t + s * s) % MOD;
+			return (t * t + s * s) % MOD;
+		}
+	}
+}
+
+int main(void)
+{
+	map <ll, ll> m;
+	m[0] = 0;
+	m[1] = 1;
+	ll a, b;
+	cin >> a >> b;
+
+	while (b%a != 0)
+	{
+		ll temp = b % a;
+		b = a;
+		a = temp;
+	}
+	cout << fibo(a, &m) % MOD;
+	return 0;
+}
