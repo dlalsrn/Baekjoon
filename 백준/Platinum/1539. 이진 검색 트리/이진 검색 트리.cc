4adff5cc32dab121f53fd4bb8295b521a8c7fae8
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2022-10-18 20:29:11 +0900

    [Platinum IV] Title: 이진 검색 트리, Time: 164 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Platinum/1539. 이진 검색 트리/이진 검색 트리.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <math.h>
+using namespace std;
+typedef long long ll;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int N, Max;
+	long long num, left, right, total = 0;
+	set <pair<ll, ll>> s;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> num;
+		if (i == 0)
+		{
+			s.insert({ num, 1 });
+			total += 1;
+		}
+		else
+		{
+			auto it = s.lower_bound(pair<ll, ll>(num, 0));
+			if (it != s.begin())
+			{
+				it--;
+				left = (*it).second;
+				it++;
+			}
+			else
+				left = 0;
+			if (it != s.end())
+			{
+				right = (*it).second;
+			}
+			else
+				right = 0;
+			Max = max(left, right) + 1;
+			total += Max;
+			s.insert({ num, Max });
+		}
+	}
+	cout << total;
+	return 0;
+}
