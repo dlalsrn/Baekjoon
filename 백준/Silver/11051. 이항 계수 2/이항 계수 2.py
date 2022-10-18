0e408a9d5a4389b9de823e11118f28097f0bab92
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2022-10-18 21:18:22 +0900

    [Silver III] Title: 이항 계수 2, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.py
@@ -0,0 +1,25 @@
+# 이항 계수 3
+MOD = 10007
+
+def factorial(n):
+    sum = 1
+    for i in range(1, n+1, 1):
+        sum = (sum*i) % MOD
+    return sum
+
+def pow(x, n):
+    if n == 1:
+        return x % MOD
+    if n % 2 == 0:
+        return (pow(x, n//2)**2) % MOD
+    else:
+        return (pow(x, (n-1)//2)**2 * x) % MOD
+
+N, K = map(int, input().split())
+a = factorial(N)
+b = factorial(K)
+c = factorial(N-K)
+
+temp = pow(b*c, MOD-2) % MOD
+total = a*temp % MOD
+print(total)
