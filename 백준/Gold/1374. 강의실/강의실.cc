d3e52a80daa0ce1c026562a0389fa38a511d0fd0
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2022-10-18 20:28:21 +0900

    [Gold V] Title: 강의실, Time: 56 ms, Memory: 5224 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.cc
@@ -0,0 +1,41 @@
+// 강의실 배정
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	priority_queue <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> temp;
+	priority_queue <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> time;
+	int N, x, s, e, cnt = 0;
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> x >> s >> e;
+		temp.push(make_pair(s, e));
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		if (time.empty())
+		{
+			time.push(make_pair(temp.top().second, temp.top().first));
+			temp.pop();
+			cnt++;
+		}
+		else
+		{
+			if (temp.top().first >= time.top().first)
+				time.pop();
+			else
+				cnt++;
+			time.push(make_pair(temp.top().second, temp.top().first));
+			temp.pop();
+		}
+	}
+	cout << cnt;
+	return 0;
+}
