52d1f6e4d947d70a1d1aea5bf94ffec7a9098a08
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2022-10-18 20:25:57 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 52 ms, Memory: 6948 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+int cnt = 0;
+int sum = 0;
+priority_queue <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+
+void DFS(vector <vector<pair<int, int>>>& v, vector <bool>& visit, int node)
+{
+	if (!visit[node])
+	{
+		visit[node] = true;
+		while (cnt < v.size() - 1)
+		{
+			for (int i = 0; i < v[node].size(); i++)
+				if (!visit[v[node][i].first])
+					q.push(make_pair(v[node][i].second, v[node][i].first));
+			while (!q.empty() && visit[q.top().second])
+				q.pop();
+			if (q.empty())
+				break;
+			int next = q.top().second;
+			sum += q.top().first;
+			cnt++;
+			q.pop();
+			DFS(v, visit, next);
+		}
+	}
+
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int V, E;
+	int start, end, value;
+	cin >> V >> E;
+	vector <vector<pair<int, int>>> v(V+1);
+	vector <bool> visit(V + 1, false);
+	for (int i = 0; i < E; i++)
+	{
+		cin >> start >> end >> value;
+		v[start].push_back(make_pair(end, value));
+		v[end].push_back(make_pair(start, value));
+	}
+	DFS(v, visit, 1);
+	cout << sum;
+	return 0;
+}
