65be770f183aad5656745804112bc863c2406340
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2022-10-18 20:22:01 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1068. 트리/트리.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector <vector<int>> node;
+int del, cnt;
+
+void DFS(int n)
+{
+	if (n == del)
+		return;
+	if (node[n].size() == 0 || (node[n].size() == 1 && node[n][0] == del))
+	{
+		cnt++;
+		return;
+	}
+	for (int x : node[n])
+		if (x != del) 
+			DFS(x);
+}
+
+int main(void)
+{
+	int N, root, parent;
+	cin >> N;
+	node = vector<vector<int>>(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> parent;
+		if (parent == -1)
+			root = i;
+		else
+			node[parent].push_back(i);
+	}
+
+	cin >> del;
+	DFS(root);
+	cout << cnt;
+	return 0;
+}
