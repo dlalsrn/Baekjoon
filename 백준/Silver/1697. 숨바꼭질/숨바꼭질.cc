19c9e01786f4f3142ccc81a226f1aa9e6b95c7c4
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2022-10-18 20:30:02 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2520 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -0,0 +1,77 @@
+// 숨바꼭질 3
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(void)
+{
+	queue <pair<int, int>> q;
+	bool TF[100001] = { 0 };
+	int loc[100001] = { 0 };
+	int N, K;
+	cin >> N >> K;
+	q.push({ N, 0 });
+	loc[N] = 0;
+	TF[N] = true;
+	while (!q.empty())
+	{
+		pair<int, int> temp = q.front();
+		q.pop();
+		int L = temp.first;
+		int cnt = temp.second;
+		if (L - 1 >= 0)
+		{
+			if (TF[L - 1] == false)
+			{
+				TF[L - 1] = true;
+				loc[L - 1] = cnt + 1;
+				q.push({ L - 1, cnt + 1 });
+			}
+			else
+			{
+				if (loc[L - 1] > cnt + 1)
+				{
+					loc[L - 1] = cnt + 1;
+					q.push({ L - 1, cnt + 1 });
+				}
+			}
+		}
+		if (L + 1 <= 100000)
+		{
+			if (TF[L + 1] == false)
+			{
+				TF[L + 1] = true;
+				loc[L + 1] = cnt + 1;
+				q.push({ L + 1, cnt + 1 });
+			}
+			else
+			{
+				if (loc[L + 1] > cnt + 1)
+				{
+					loc[L + 1] = cnt + 1;
+					q.push({ L + 1, cnt + 1 });
+				}
+			}
+		}
+		if (L * 2 <= 100000)
+		{
+			if (TF[L * 2] == false)
+			{
+				TF[L * 2] = true;
+				loc[L * 2] = cnt+1;
+				q.push({ L * 2, cnt +1});
+			}
+			else
+			{
+				if (loc[L * 2] > cnt+1)
+				{
+					loc[L * 2] = cnt+1;
+					q.push({ L * 2, cnt +1});
+				}
+			}
+		}
+	}
+
+	cout << loc[K];
+	return 0;
+}
