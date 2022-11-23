e37c99cf449a0c28a424f9ec80d296b5c8dc5f7e
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2022-11-23 16:17:14 +0900

    [Platinum V] Title: Bolhas e Baldes, Time: 344 ms, Memory: 9376 KB -BaekjoonHub

---
+++ 백준/Platinum/13681. Bolhas e Baldes/Bolhas e Baldes.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+typedef long long ll;
+vector <ll> segtree;
+vector <pair<ll, ll>> value;
+ll T, N, cnt;
+
+void update(ll n, ll start, ll end, ll index)
+{
+	if (start == end)
+		segtree[n] = 1;
+	else
+	{
+		if (index <= (start + end) / 2)
+			update(n * 2, start, (start + end) / 2, index);
+		else
+			update(n * 2 + 1, (start + end) / 2 + 1, end, index);
+		segtree[n] = segtree[n * 2] + segtree[n * 2 + 1];
+	}
+}
+
+ll get_sum(ll n, ll start, ll end, ll left, ll right)
+{
+	if (right < start || left > end)
+		return 0;
+	if (left <= start && end <= right)
+		return segtree[n];
+	return get_sum(n * 2, start, (start + end) / 2, left, right) + get_sum(n * 2 + 1, (start + end) / 2 + 1, end, left, right);
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	while(cin >> N)
+	{
+		if (N == 0)
+			break;
+		ll h = (ll)ceil(log2(N));
+
+		segtree = vector<ll>((1 << (h + 1)), 0);
+		value = vector<pair<ll, ll>>(N);
+
+		for (ll i = 0; i < N; i++)
+		{
+			cin >> value[i].first;
+			value[i].second = i;
+		}
+
+		sort(value.begin(), value.end());
+
+		cnt = 0;
+		for (ll i = 0; i < N; i++)
+		{
+			cnt += get_sum(1, 0, N - 1, value[i].second + 1, N - 1);
+			update(1, 0, N - 1, value[i].second);
+		}
+
+		cout << (cnt & 1 ? "Marcelo" : "Carlos") << '\n';
+	}
+
+	return 0;
+}
