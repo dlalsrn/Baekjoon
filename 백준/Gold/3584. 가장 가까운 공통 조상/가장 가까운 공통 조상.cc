d632b7f5da035a6380a8b87b087217a883816224
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2023-04-03 13:06:05 +0900

    [Gold IV] Title: 가장 가까운 공통 조상, Time: 8 ms, Memory: 2992 KB -BaekjoonHub

---
+++ 백준/Gold/3584. 가장 가까운 공통 조상/가장 가까운 공통 조상.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int T, N, u, v, root;
+vector <vector<int>> node;
+vector <int> parent;
+vector <int> depth;
+
+void DFS(int r, int d)
+{
+	depth[r] = d;
+	for (int i = 0; i < node[r].size(); i++)
+		DFS(node[r][i], d + 1);
+}
+
+int LCA(int u, int v)
+{
+	while (u != v)
+	{
+		if (depth[u] > depth[v])
+			u = parent[u];
+		else
+			v = parent[v];
+	}
+	return u;
+}
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> T;
+
+	for (int t = 0; t < T; t++)
+	{
+		cin >> N;
+		
+		parent = vector<int>(N + 1, -1);
+		depth = vector<int>(N + 1, 0);
+		node = vector<vector<int>>(N + 1);
+
+		for (int i = 0; i < N - 1; i++)
+		{
+			cin >> u >> v;
+			node[u].push_back(v);
+			parent[v] = u;
+		}
+
+		for (int i = 1; i <= N; i++)
+		{
+			if (parent[i] == -1)
+			{
+				root = i;
+				break;
+			}
+		}
+
+		DFS(root, 0);
+
+		cin >> u >> v;
+		int lca = LCA(u, v);
+
+		cout << lca << '\n';
+	}
+
+	return 0;
+}
