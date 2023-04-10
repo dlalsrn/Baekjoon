7460b2c3fc996ae4b30caa2fa19746aff4dbe05d
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2023-04-10 16:48:25 +0900

    [Silver II] Title: A → B, Time: 8 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.cc
+++ 백준/Silver/16953. A → B/A → B.cc
@@ -1,64 +1,40 @@
 // A -> B
 #include <iostream>
 #include <queue>
-#include <map>
 #include <string>
 using namespace std;
 typedef long long ll;
 
+ll A, B;
+queue <pair<ll, int>> q;
+
 int main(void)
 {
-	map <ll, int> m;
-	queue <pair<ll, int>> q;
-	ll A;
-	int B;
 	cin >> A >> B;
+
 	q.push({ A, 1 });
-	m[A] = 1;
+	
 	while (!q.empty())
 	{
-		pair<ll, int> temp = q.front();
+		ll num = q.front().first, cnt = q.front().second;
 		q.pop();
-		ll num = temp.first;
-		int cnt = temp.second;
-		ll plus_1 = stoll(to_string(num) + "1");
-		ll mult_2 = num * 2;
-		if (plus_1 <= B)
+		if (num > B)
+			continue;
+		ll plus = stoll(to_string(num) + "1");
+		ll mult = num * 2;
+
+		if (num == B)
 		{
-			if (!m[plus_1])
-			{
-				m[plus_1] = cnt+1;
-				q.push({ plus_1, cnt + 1 });
-			}
-			else
-			{
-				if (m[plus_1] > cnt + 1)
-				{
-					m[plus_1] = cnt + 1;
-					q.push({ plus_1, cnt + 1 });
-				}
-			}
+			cout << cnt;
+			return 0;
 		}
-		if (mult_2 <= B)
-		{
-			if (!m[mult_2])
-			{
-				m[mult_2] = cnt + 1;
-				q.push({ mult_2, cnt + 1 });
-			}
-			else
-			{
-				if (m[mult_2] > cnt + 1)
-				{
-					m[mult_2] = cnt + 1;
-					q.push({ mult_2, cnt + 1 });
-				}
-			}
-		}
+		if (plus <= B)
+			q.push(make_pair(plus, cnt + 1));
+		if (mult <= B)
+			q.push(make_pair(mult, cnt + 1));
 	}
-	if (m[B])
-		cout << m[B];
-	else
-		cout << -1;
+
+	cout << -1;
+
 	return 0;
 }
