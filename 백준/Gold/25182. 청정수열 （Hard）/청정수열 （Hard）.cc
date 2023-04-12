a995f4bdc51d36727b145607aec7525f37591bd1
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2023-04-12 15:51:44 +0900

    [Gold II] Title: 청정수열 (Hard), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/25182. 청정수열 （Hard）/청정수열 （Hard）.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#define MOD 1000000007
+using namespace std;
+
+typedef long long ll;
+ll N, sum = 0, ans = 0, cnt = 0, fac = 1;
+
+int main(void)
+{
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+	{
+		sum = (sum + i * 2) % MOD;
+		ans = (ans + sum * i) % MOD;
+	}
+
+	for (int i = 2; i <= N; i++)
+		fac = (fac * i) % MOD;
+
+	cout << ans << ' ' << fac * fac % MOD;
+
+	return 0;
+}
