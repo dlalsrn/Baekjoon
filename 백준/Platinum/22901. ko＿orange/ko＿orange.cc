9b58b8957f19b7943ce8784b07056fb40242b97b
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2023-04-18 20:36:00 +0900

    [Platinum II] Title: ko_orange, Time: 372 ms, Memory: 13948 KB -BaekjoonHub

---
+++ 백준/Platinum/22901. ko＿orange/ko＿orange.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+int last_mn, mx, mn, T, x;
+
+int main(void)
+{
+	cin >> T;
+
+	for (int t = 0; t < T; t++)
+	{
+		mx = 2399, mn = 2100, last_mn = 2100;
+		for (int i = 0; i < 4; i++)
+		{
+			int mid = (mx + mn + 1) / 2;
+			cout << "? " << mid << endl;
+			cin >> x;
+
+			if (x == 1)
+			{
+				last_mn = mn;
+				mn = mid;
+			}
+			else
+				mx = mid - 1;
+		}
+
+		mn = last_mn;
+
+		while(mx - mn != 0)
+		{
+			int mid = (mx + mn + 1) / 2;
+			cout << "? " << mid << endl;
+			cin >> x;
+
+			if (x == 1)
+			{
+				last_mn = mn;
+				mn = mid;
+			}
+			else
+				mx = mid - 1;
+			if (mx - mn == 0)
+			{
+				cout << "? " << mn << endl;
+				cin >> x;
+				if (x == 1)
+					break;
+				else
+					mn = last_mn;
+			}
+		}
+
+		cout << "! " << mn << endl;
+	}
+}
