ebcde9b6758b715755652282380518ef10b5baa7
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2023-04-19 13:31:54 +0900

    [Silver I] Title: OX, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/27970. OX/OX.cc
+++ 백준/Silver/27970. OX/OX.cc
@@ -1,27 +1,2 @@
-#include <iostream>
-#include <vector>
-#define MOD 1000000007
-using namespace std;
-typedef long long ll;
-
-vector <ll> s(100000);
-string x;
-ll sum = 0;
-
-int main(void)
-{
-	cin >> x;
-
-	s[0] = 0;
-
-	for (ll i = 1; i < 100000; i++)
-		s[i] = (s[i - 1] * 2 + 1) % MOD;
-
-	for (int i = 0; i < x.size(); i++)
-		if (x[i] == 'O')
-			sum = (sum + s[i] + 1) % MOD;
-
-	cout << sum;
-
-	return 0;
-}
+#include<iostream>
+int m=1e9+7,s=0,g=1;main(){char a;while((a=getchar())!='\n'){s=(s+(a=='O')*g)%m;g=(g*2)%m;}printf("%d",s);}
