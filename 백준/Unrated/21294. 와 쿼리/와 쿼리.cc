09cf5da0aa3a2c98f3a28648013e05c6e432daff
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2023-04-19 18:38:10 +0900

    [Unrated] Title: 와 쿼리, Time: 76 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Unrated/21294. 와 쿼리/와 쿼리.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int N = 0, Q, ch;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cin >> Q;
+
+	for (int i = 0; i < Q; i++)
+	{
+		cin >> ch;
+		if (ch == 1);
+		else
+			cout << '\n';
+	}
+
+	return 0;
+}
