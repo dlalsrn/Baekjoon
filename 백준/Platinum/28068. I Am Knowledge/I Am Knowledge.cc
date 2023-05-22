4ee9ef716cdf6fed0879729538b31237a85c6c0e
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2023-05-22 14:19:54 +0900

    [Platinum IV] Title: I Am Knowledge, Time: 48 ms, Memory: 6244 KB -BaekjoonHub

--- 백준/Platinum/28068. I Am Knowledge/I Am Knowledge.cc
+++ 백준/Platinum/28068. I Am Knowledge/I Am Knowledge.cc
@@ -1,10 +1,8 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 using namespace std;
 using ll = long long;
 using pi = pair<ll, ll>;
-using pii = pair<ll, pair<ll, ll>>;
 
 struct cmp1
 {
@@ -29,8 +27,6 @@ struct cmp2
 };
 
 ll N, a, b, sum = 0;
-vector <ll> v1; // 소모
-vector <ll> v2; // 얻음
 priority_queue<pi, vector<pi>, cmp2> keep;
 priority_queue<pi, vector<pi>, cmp1> pq;
 
@@ -43,12 +39,8 @@ int main(void)
 	for (int i = 0; i < N; i++)
 	{
 		cin >> a >> b;
-		v1.push_back(a);
-		v2.push_back(b);
+		pq.push(make_pair(a, b));
 	}
-
-	for (int i = 0; i < N; i++)
-		pq.push(make_pair(v1[i], v2[i]));
 
 	while (!pq.empty())
 	{
@@ -67,14 +59,10 @@ int main(void)
 			sum += (t.second - t.first);
 	}
 
-	//cout << sum << '\n';
-
 	while (!keep.empty())
 	{
 		pi t = keep.top();
 		keep.pop();
-
-		//cout << t.first << ' ' << t.second << '\n';
 
 		if (sum - t.first < 0)
 		{
