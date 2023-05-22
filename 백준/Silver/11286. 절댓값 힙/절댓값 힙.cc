b8ddc935086840c174f6bb0a4e7bdb1d0b4c42fd
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2023-05-22 13:11:01 +0900

    [Silver I] Title: 절댓값 힙, Time: 848 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -2,26 +2,28 @@
 #include <queue>
 using namespace std;
 
+priority_queue <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+int N, x;
+
 int main(void)
 {
-	priority_queue <pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> q;
-	int N, x;
 	cin >> N;
 	for (int i = 0; i < N; i++)
 	{
 		cin >> x;
-		if (x == 0)
+		if (x)
+			q.push(make_pair(abs(x), x));
+		else
 		{
-			if (!q.empty())
+			if (q.empty())
+				cout << "0\n";
+			else
 			{
 				cout << q.top().second << '\n';
 				q.pop();
 			}
-			else
-				cout << "0\n";
 		}
-		else
-			q.push({abs(x), x});
 	}
+
 	return 0;
 }
