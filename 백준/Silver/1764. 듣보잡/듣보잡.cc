3ed1d5541773596053b1502c4b162f35d5139ff9
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2023-05-31 14:43:21 +0900

    [Silver IV] Title: 듣보잡, Time: 116 ms, Memory: 12860 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -1,39 +1,30 @@
-#include <string>
 #include <iostream>
-#include <algorithm>
+#include <map>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+map <string, int> m;
+vector <string> v;
+string x;
+int N, M;
 
 int main(void)
 {
-	int A, B;
-	cin >> A >> B;
-	string a;
-	vector <string> v1;
-	vector <string> v2;
+	cin >> N >> M;
 
-	for (int i = 0; i < A; i++)
+	for (int i = 0; i < N + M; i++)
 	{
-		cin >> a;
-		v1.push_back(a);
-	}
-	for (int i = 0; i < B; i++)
-	{
-		cin >> a;
-		v1.push_back(a);
+		cin >> x;
+		if (++m[x] == 2)
+			v.push_back(x);
 	}
 
-	sort(v1.begin(), v1.end());
-	for (int i = 0; i < A + B; i++)
-	{
-		if (v1[i] == v1[i + 1])
-		{
-			v2.push_back(v1[i]);
-			i++;
-		}
-	}
-	cout << v2.size() << '\n';
-	for (int i = 0; i < v2.size(); i++)
-		cout << v2[i] << '\n';
-    return 0;
+	sort(v.begin(), v.end());
+
+	cout << v.size() << '\n';
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << '\n';
+
+	return 0;
 }
