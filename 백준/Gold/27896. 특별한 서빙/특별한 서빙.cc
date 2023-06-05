bca18aaf62a10f4c7241762c1f2990bf44341844
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2023-06-05 11:04:58 +0900

    [Gold V] Title: 특별한 서빙, Time: 124 ms, Memory: 3568 KB -BaekjoonHub

--- 백준/Gold/27896. 특별한 서빙/특별한 서빙.cc
+++ 백준/Gold/27896. 특별한 서빙/특별한 서빙.cc
@@ -2,30 +2,27 @@
 #include <queue>
 using namespace std;
 
-long long n, m, y;
-long long tmp = 0, egg = 0;
-queue<int> q;               //급식 순서
+long long N, M, Y, x, cnt = 0, sum = 0;
 priority_queue<int> pq;
 
-int main() {
-    cin >> n >> m;
-    for (int i = 0; i < n; i++) {
-        cin >> y;
-        q.push(y);
-    }
+int main(void)
+{
+    cin >> N >> M;
 
-    while (!q.empty()) {
-        pq.push(q.front());     //급식 배식 시작
-        tmp += q.front(); // 불만도 더하기
+    for (int i = 0; i < N; i++) 
+    {
+        cin >> x;
+        pq.push(x);
+        sum += x;
 
-        if (tmp >= m) {  //가지 배식
-            egg++;
-            tmp -= pq.top() * 2;
+        if (sum >= M) {
+            cnt++;
+            sum -= pq.top() * 2;
             pq.pop();
         }
-
-        q.pop();                //급식 배식 완료
     }
-    cout << egg;
+
+    cout << cnt;
+
     return 0;
 }
