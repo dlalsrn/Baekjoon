77fa80d51390cd00ae8e80a70d26033e2031fe54
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2023-11-10 21:19:08 +0900

    [Gold III] Title: Almost Always, Time: 156 ms, Memory: 15632 KB -BaekjoonHub

--- 백준/Gold/23364. Almost Always/Almost Always.cc
+++ 백준/Gold/23364. Almost Always/Almost Always.cc
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, a, b;
-vector <int> v;
+int N, x;
 map <int, int> m;
 
 int main(void)
@@ -10,19 +9,15 @@ int main(void)
 	cin.tie(0)->sync_with_stdio(false);
 	cin >> N;
 
-	v = vector<int>(N);
-
-	for (int i = 0; i < N; i++)
-		cin >> v[i];
-
 	for (int i = 0; i < N; i++)
 	{
-		if (m.find(v[i]) != m.end())
+		cin >> x;
+		if (m.find(x) != m.end())
 		{
-			cout << i + 1 << ' ' << m[v[i]];
+			cout << i + 1 << ' ' << m[x];
 			break;
 		}
-		m[v[i]] = i + 1;
+		m[x] = i + 1;
 	}
 
 	return 0;
