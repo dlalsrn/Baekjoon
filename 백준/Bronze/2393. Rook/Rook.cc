9b04b134ad7cebd77c677d5cf76494656d89250a
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2023-12-04 14:31:36 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(void)
+{
+	cin.tie(0)->sync_with_stdio(false);
+
+	cout << "  ___  ___  ___\n";
+	cout << "  | |__| |__| |\n";
+	cout << "  |           |\n";
+	cout << "   \\_________/\n";
+	cout << "    \\_______/\n";
+	cout << "     |     |\n";
+	cout << "     |     |\n";
+	cout << "     |     |\n";
+	cout << "     |     |\n";
+	cout << "     |_____|\n";
+	cout << "  __/       \\__\n";
+	cout << " /             \\\n";
+	cout << "/_______________\\";
+
+	return 0;
+}
