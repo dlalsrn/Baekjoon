98ac8e23d4b1698a6a2d1079c494bf0ef9be2a48
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2024-02-01 19:41:16 +0900

    [Silver III] Title: 문제 출제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/26163. 문제 출제/문제 출제.cc
+++ 백준/Silver/26163. 문제 출제/문제 출제.cc
@@ -13,42 +13,19 @@ int main(void)
 
 	for (int n = 1; n <= 15; n++)
 	{
-		if (n <= 3)
+		for (int a = 0; a <= n; a++)
 		{
-			for (int a = 0; a <= n; a++)
+			for (int b = 0; a + b <= n; b++)
 			{
-				for (int b = 0; a + b <= n; b++)
+				for (int c = 0; a + b + c <= n; c++)
 				{
-					for (int c = 0; a + b + c <= n; c++)
+					for (int d = 0; a + b + c + d <= n; d++)
 					{
-						for (int d = 0; a + b + c + d <= n; d++)
-						{
-							int e = n - (a + b + c + d);
-							int t = a * 1 + b * 2 + c * 3 + d * 4 + e * 5;
-							if (t > 10)
-								continue;
-							mx = max(mx, a * v[0] + b * v[1] + c * v[2] + d * v[3] + e * v[4]);
-						}
-					}
-				}
-			}
-		}
-		else
-		{
-			for (int a = 0; a <= n; a++)
-			{
-				for (int b = 0; a + b <= n; b++)
-				{
-					for (int c = 0; a + b + c <= n; c++)
-					{
-						for (int d = 0; a + b + c + d <= n; d++)
-						{
-							int e = n - (a + b + c + d);
-							int t = a * 1 + b * 2 + c * 3 + d * 4 + e * 5;
-							if (t > 15)
-								continue;
-							mx = max(mx, a * v[0] + b * v[1] + c * v[2] + d * v[3] + e * v[4]);
-						}
+						int e = n - (a + b + c + d);
+						int t = a * 1 + b * 2 + c * 3 + d * 4 + e * 5;
+						if ((n <= 3 && t > 10) || t > 15)
+							continue;
+						mx = max(mx, a * v[0] + b * v[1] + c * v[2] + d * v[3] + e * v[4]);
 					}
 				}
 			}
