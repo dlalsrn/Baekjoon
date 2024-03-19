f261323dce9cc0246ba3989af6d8d2aab4d29e0a
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2024-03-19 17:00:51 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+	cout << 1999 << '\n';
+	
+	for (int i = 0; i < 999; i++) cout << 1 << ' ';
+	for (int i = 0; i < 1000; i++) cout << 1000 << ' ';
+
+	return 0;
+}
