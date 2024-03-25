9f844204fa69346a6d16321df5720062771577d7
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2024-03-25 10:50:12 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 44 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -1,25 +1,24 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+
+int N, a, b;
+vector <pair<int, int>> v;
 
 int main(void)
 {
-	vector<pair<int, int>> v;
-	int N;
+	cin.tie(0)->sync_with_stdio(false);
+
 	cin >> N;
 
 	for (int i = 0; i < N; i++)
 	{
-		int a, b;
 		cin >> a >> b;
 		v.push_back(make_pair(a, b));
 	}
 
 	sort(v.begin(), v.end());
 
-	for (int i = 0; i < N; i++)
-		cout << v[i].first << ' ' << v[i].second << '\n';
+	for (int i = 0; i < N; i++) cout << v[i].first << ' ' << v[i].second << '\n';
 
 	return 0;
 }
