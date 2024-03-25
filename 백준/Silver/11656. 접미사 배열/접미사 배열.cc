b4d4b3e21e6c00e5365f8eacd1031e72f7d96def
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2024-03-25 11:00:38 +0900

    [Silver IV] Title: 접미사 배열, Time: 4 ms, Memory: 2692 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.cc
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.cc
@@ -1,22 +1,24 @@
-#include <string>
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
+
+string x, t = "";
+vector <string> v;
 
 int main(void)
 {
-	string a;
-	int n;
-	vector <string> v;
-	cin >> a;
-	n = a.size();
+	cin.tie(0)->sync_with_stdio(false);
 
-	for (int i = 0; i < n; i++)
-		v.push_back(a.substr(i));
+	cin >> x;
+
+	for (int i = x.size() - 1; i >= 0; i--)
+	{
+		t = x[i] + t;
+		v.push_back(t);
+	}
 
 	sort(v.begin(), v.end());
-	for (int i = 0; i < n; i++)
-		cout << v[i] << '\n';
-    return 0;
+
+	for (int i = 0; i < v.size(); i++) cout << v[i] << '\n';
+
+	return 0;
 }
