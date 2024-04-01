15afe26f209ae15dab9a38ab5d7987644710ad8d
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2024-04-01 17:57:50 +0900

    [Silver IV] Title: 제로, Time: 8 ms, Memory: 2548 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.cc
+++ 백준/Silver/10773. 제로/제로.cc
@@ -1,5 +1,4 @@
-#include <iostream>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
 
 int K, x, sum = 0;
@@ -7,9 +6,11 @@ stack <int> s;
 
 int main(void)
 {
+    cin.tie(0)->sync_with_stdio(false);
+
     cin >> K;
 
-    for (int i = 0; i < K; i++)
+    for(int i = 0; i < K; i++)
     {
         cin >> x;
 
