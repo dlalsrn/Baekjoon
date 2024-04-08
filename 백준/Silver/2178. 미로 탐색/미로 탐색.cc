09d4981237e29c78c81b2ca06cf66b3e117c7865
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2024-04-08 10:13:05 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,41 +1,39 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
 int N, M;
 string x;
-vector <string> map;
+vector <string> m;
 vector <vector<bool>> check;
-queue <pair<int, pair<int, int>>> q;
+queue <pair<pair<int, int>, int>> q;
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
 
-void BFS()
+void bfs()
 {
 	while (!q.empty())
 	{
-		int y = q.front().second.first;
-		int x = q.front().second.second;
-		int c = q.front().first;
+		int y = q.front().first.first;
+		int x = q.front().first.second;
+		int c = q.front().second;
+		q.pop();
+
 		if (y == N - 1 && x == M - 1)
 		{
 			cout << c;
-			q = queue <pair<int, pair<int, int>>>();
-			continue;
+			return;
 		}
-		q.pop();
-		for (int i = 0; i < 4; i++)
+
+		for (int k = 0; k < 4; k++)
 		{
-			int yy = y + dy[i];
-			int xx = x + dx[i];
-			if (yy < 0 || xx < 0 || xx >= M || yy >= N || check[yy][xx] || map[yy][xx] == '0')
+			int yy = y + dy[k];
+			int xx = x + dx[k];
+
+			if (yy < 0 || xx < 0 || xx >= M || yy >= N || check[yy][xx] || m[yy][xx] == '0')
 				continue;
 
 			check[yy][xx] = true;
-			q.push(make_pair(c + 1, make_pair(yy, xx)));
+			q.push(make_pair(make_pair(yy, xx), c + 1));
 		}
 	}
 }
@@ -52,13 +50,13 @@ int main(void)
 	for (int i = 0; i < N; i++)
 	{
 		cin >> x;
-		map.push_back(x);
+		m.push_back(x);
 	}
 
-	q.push(make_pair(1, make_pair(0, 0)));
 	check[0][0] = true;
+	q.push(make_pair(make_pair(0, 0), 1));
 
-	BFS();
+	bfs();
 
 	return 0;
 }
