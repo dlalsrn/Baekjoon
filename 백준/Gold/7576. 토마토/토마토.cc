49b207c8f635d03b7ea615c1dbf78287ddf84f0e
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2024-04-08 09:34:50 +0900

    [Gold V] Title: 토마토, Time: 104 ms, Memory: 12696 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.cc
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -1,76 +1,72 @@
-#include <iostream>
-#include <memory.h>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
+
+int N, M, x, mx = 0;
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+vector <vector<int>> m;
+vector <vector<bool>> check;
+queue <pair<pair<int, int>, int>> q;
+
+void bfs()
+{
+	while (!q.empty())
+	{
+		int y = q.front().first.first;
+		int x = q.front().first.second;
+		int cnt = q.front().second;
+		q.pop();
+		mx = max(mx, cnt);
+
+		for (int k = 0; k < 4; k++)
+		{
+			int xx = x + dx[k];
+			int yy = y + dy[k];
+			if (xx < 0 || xx >= M || yy < 0 || yy >= N || check[yy][xx] || m[yy][xx] == -1)
+				continue;
+			check[yy][xx] = true;
+			q.push(make_pair(make_pair(yy, xx), cnt + 1));
+		}
+	}
+}
 
 int main(void)
 {
-	int M, N, i, j, cnt = 0;
-	bool TF[1000][1000];
-	int tmt[1000][1000];
-	queue <pair<int, pair<int, int>>> q;
+	cin.tie(0)->sync_with_stdio(false);
+
 	cin >> M >> N;
-	for (i = 0; i < N; i++)
+
+	m.resize(N, vector<int>(M));
+	check.resize(N, vector<bool>(M, false));
+
+	for (int i = 0; i < N; i++)
 	{
-		for (j = 0; j < M; j++)
+		for (int j = 0; j < M; j++)
 		{
-			cin >> tmt[i][j];
-			if (tmt[i][j] == 1)
+			cin >> m[i][j];
+			if (m[i][j] == 1)
 			{
-				TF[i][j] = true;
-				q.push({ 0, {i,j} });
+				q.push(make_pair(make_pair(i, j), 0));
+				check[i][j] = true;
 			}
-			else
-				TF[i][j] = false;
-		}
-	}
-	//cout << q.size();
-	pair <int, pair<int, int>> temp;
-	while (!q.empty())
-	{
-		temp = q.front();
-		i = temp.second.first;
-		j = temp.second.second;
-		cnt = temp.first;
-		q.pop();
-
-		if (i + 1 < N && TF[i + 1][j] == false && tmt[i + 1][j] == 0)
-		{
-			TF[i + 1][j] = true;
-			tmt[i + 1][j] = 1;
-			q.push({ cnt + 1, { i + 1, j } });
-		}
-		if (i - 1 >= 0 && TF[i - 1][j] == false && tmt[i - 1][j] == 0)
-		{
-			TF[i - 1][j] = true;
-			tmt[i - 1][j] = 1;
-			q.push({ cnt + 1, { i - 1, j } });
-		}
-		if (j + 1 < M && TF[i][j + 1] == false && tmt[i][j + 1] == 0)
-		{
-			TF[i][j + 1] = true;
-			tmt[i][j + 1] = 1;
-			q.push({ cnt + 1,{ i, j + 1 } });
-		}
-		if (j - 1 >= 0 && TF[i][j - 1] == false && tmt[i][j - 1] == 0)
-		{
-			TF[i][j - 1] = true;
-			tmt[i][j - 1] = 1;
-			q.push({ cnt + 1, {i, j - 1 } });
 		}
 	}
 
-	for (i = 0; i < N; i++)
+	bfs();
+
+	for (int i = 0; i < N; i++)
 	{
-		for (j = 0; j < M; j++)
+		for (int j = 0; j < M; j++)
 		{
-			if (TF[i][j] == false && tmt[i][j] != -1)
+			if (!check[i][j] && m[i][j] != -1)
 			{
 				cout << -1;
 				return 0;
 			}
 		}
 	}
-	cout << cnt;
+
+	cout << mx;
+
 	return 0;
 }
