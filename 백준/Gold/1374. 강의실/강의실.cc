e10efd2190c7af7eaa9345b9abae16026f13bcd6
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2024-04-27 16:27:08 +0900

    [Gold V] Title: 강의실, Time: 52 ms, Memory: 5220 KB -BaekjoonHub

--- 백준/Gold/1374. 강의실/강의실.cc
+++ 백준/Gold/1374. 강의실/강의실.cc
@@ -1,41 +1,37 @@
-// 강의실 배정
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
+using pi = pair<int, int>;
+
+int N, x, s, e, cnt = 0;
+priority_queue <pi, vector<pi>, greater<pi>> cls, room;
 
 int main(void)
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	priority_queue <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> temp;
-	priority_queue <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> time;
-	int N, x, s, e, cnt = 0;
+	cin.tie(0)->sync_with_stdio(false);
+
 	cin >> N;
+
 	for (int i = 0; i < N; i++)
 	{
 		cin >> x >> s >> e;
-		temp.push(make_pair(s, e));
+		cls.push(make_pair(s, e));
 	}
 
 	for (int i = 0; i < N; i++)
 	{
-		if (time.empty())
-		{
-			time.push(make_pair(temp.top().second, temp.top().first));
-			temp.pop();
-			cnt++;
-		}
+		if (room.empty()) cnt++; // 맨 처음 강의
 		else
 		{
-			if (temp.top().first >= time.top().first)
-				time.pop();
-			else
-				cnt++;
-			time.push(make_pair(temp.top().second, temp.top().first));
-			temp.pop();
+			// 수업하고 있는 강의 중 가장 빨리 끝나는 강의의 시간이 다음 수업의 시작 시간보다 이르면 
+			// 즉, 수업 시간이 겹치지 않는 다면 강의실을 하나 더 배정할 필요가 없음
+			if (cls.top().first >= room.top().first) room.pop();
+			else cnt++;
 		}
+		room.push(make_pair(cls.top().second, cls.top().first));
+		cls.pop();
 	}
+
 	cout << cnt;
+
 	return 0;
 }
