6920dd5a8105642873cb1ddecc6e61f3bb38e5f7
LeeMingu <56715189+dlalsrn@users.noreply.github.com>
2024-11-19 16:16:46 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -1,77 +1,65 @@
-#include <iostream>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
+
+int N, M, B, W, ans;
+char first;
+bool TF;
+string x;
+vector <string> v;
 
 int main(void)
 {
-	priority_queue <int, vector<int>, greater<int>> q;
-	int N, M, cnt;
-	char check;
-	char chess[51][51];
+	cin.tie(0)->sync_with_stdio(false);
+	
 	cin >> N >> M;
 
-	for (int i = 0; i < N; i++)
-		cin >> chess[i];
+	for (int i = 0; i < N; i++) {
+		cin >> x;
+		v.push_back(x);
+	}
 
-	for (int i = 0; i <= N-8; i++)
-	{
-		for (int j = 0; j <= M-8; j++)
-		{
-			for (int g = 0; g < 4; g++)
-			{
-				cnt = 0;
-				if (g == 0)
-					check = chess[i][j];
-				else if (g == 1)
-				{
-					check = chess[i][j + 7];
-					if (check == 'B')
-						check = 'W';
-					else
-						check = 'B';
-				}
-				else if (g == 2)
-				{
-					check = chess[i + 7][j];
-					if (check == 'B')
-						check = 'W';
-					else
-						check = 'B';
-				}
-				else
-					check = chess[i + 7][j + 7];
-				for (int k = 0; k < 8; k++)
-				{
-					for (int h = 0; h < 8; h++)
-					{
-						if (k % 2 == 0)
-						{
-							if (h % 2 == 0)
-							{
-								if (chess[k + i][h + j] != check)
-									cnt++;
-							}
-							else
-								if (chess[k + i][h + j] == check)
-									cnt++;
-						}
-						else
-						{
-							if (h % 2 == 0)
-							{
-								if (chess[k + i][h + j] == check)
-									cnt++;
-							}
-							else
-								if (chess[k + i][h + j] != check)
-									cnt++;
-						}
+	ans = N * M;
+
+	for (int a = 0; a <= N - 8; a++) {
+		for (int b = 0; b <= M - 8; b++) {
+			TF = true;
+			B = W = 0;
+
+			for (int i = a; i < a + 8; i++) {
+				bool check = TF;
+				for (int j = b; j < b + 8; j++) {
+					if (check) {
+						if (v[i][j] == 'W') B++;
 					}
+					else {
+						if (v[i][j] == 'B') W++;
+					}
+					check = !check;
 				}
-				q.push(cnt);
+				TF = !TF;
 			}
+			ans = min(ans, B + W);
+			
+			TF = false;
+			B = W = 0;
+			for (int i = a; i < a + 8; i++) {
+				bool check = TF;
+				for (int j = b; j < b + 8; j++) {
+					if (check) {
+						if (v[i][j] == 'W') B++;
+					}
+					else {
+						if (v[i][j] == 'B') W++;
+					}
+					check = !check;
+				}
+				TF = !TF;
+			}
+			ans = min(ans, B + W);
 		}
 	}
-	cout << q.top();
+
+	cout << ans;
+
 	return 0;
 }
