f261a4d0dcc574f2bc9e2b1aed911a346588f64d
LEE MINGU <56715189+dlalsrn@users.noreply.github.com>
2025-08-08 20:53:22 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.01 ms, Memory: 4.05 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+	long long answer = 0;
+
+	for (int i = 1; i <= count; i++)
+	{
+		answer += price * i;
+	}
+
+	return (answer - money < 0 ? 0 : answer - money);
+}
