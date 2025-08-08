47bd9f3a4f98a0ff6a70bae2b810634d3e01e737
LEE MINGU <56715189+dlalsrn@users.noreply.github.com>
2025-08-08 16:10:12 +0900

    [level 1] Title: 약수의 합, Time: 0.03 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = n;
+    
+    for (int i = 1; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            answer += i;
+        }
+    }
+    return answer;
+}
