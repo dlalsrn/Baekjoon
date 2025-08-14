60614dbbea4a2a4228382cf245f05168baf7888f
LEE MINGU <56715189+dlalsrn@users.noreply.github.com>
2025-08-14 09:19:41 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string) {
+    string answer = "";
+    return answer = string(my_string.rbegin(), my_string.rend());
+}
