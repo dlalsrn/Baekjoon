5e36fa5955e45f70ace89931c420828e433fbd8b
LEE MINGU <56715189+dlalsrn@users.noreply.github.com>
2025-08-26 20:37:16 +0900

    [level 2] Title: 귤 고르기, Time: 9.88 ms, Memory: 7.04 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -1,19 +1,46 @@
-#include <bits/stdc++.h>
-
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int solution(int k, vector<int> tangerine) {
+int solution(int k, vector<int> tangerine)
+{
     int answer = 0;
-    int m = *max_element(tangerine.begin(), tangerine.end());
-    vector<int> v(m, 0);
-    for(auto& t : tangerine){
-        v[t - 1]++;
+    vector<int> v;
+
+    sort(tangerine.begin(), tangerine.end());
+    int cnt = 1;
+    int n = tangerine[0];
+
+    for (int i = 1; i < tangerine.size(); i++)
+    {
+        if (n == tangerine[i])
+        {
+            cnt++;
+        }
+        else
+        {
+            v.push_back(cnt);
+            cnt = 1;
+            n = tangerine[i];
+        }
     }
-    stable_sort(v.rbegin(), v.rend());
-    for(int i = 0 ; i < v.size() ; i++){
-        answer++;
-        k -= v[i];
-        if(k <= 0) return answer;
+
+    v.push_back(cnt);
+
+    sort(v.begin(), v.end(), greater<>());
+
+    cnt = 0;
+    for (int i = 0; i < v.size(); i++)
+    {
+        cnt += v[i];
+
+        if (cnt >= k)
+        {
+            answer = i + 1;
+            break;
+        }
     }
+
     return answer;
 }
