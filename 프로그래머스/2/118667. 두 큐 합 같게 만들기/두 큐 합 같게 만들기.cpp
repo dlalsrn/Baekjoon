42bd1728c70f35570727a861689825f9120aa0b4
LEE MINGU <56715189+dlalsrn@users.noreply.github.com>
2025-09-08 09:26:48 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 5.42 ms, Memory: 25.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+using ll = long long;
+
+int solution(vector<int> queue1, vector<int> queue2)
+{
+    int answer = -1;
+    ll sum1 = 0;
+    ll sum2 = 0;
+    int cnt = 0;
+    queue<int> q1;
+    queue<int> q2;
+
+    for (int i = 0; i < queue1.size(); i++)
+    {
+        sum1 += queue1[i];
+        q1.push(queue1[i]);
+        sum2 += queue2[i];
+        q2.push(queue2[i]);
+    }
+
+    for (int i = 0; i < queue1.size() * 4 + 1; i++)
+    {
+        if (sum1 < sum2)
+        {
+            sum1 += q2.front();
+            q1.push(q2.front());
+
+            sum2 -= q2.front();
+            q2.pop();
+        }
+        else if (sum1 > sum2)
+        {
+            sum2 += q1.front();
+            q2.push(q1.front());
+
+            sum1 -= q1.front();
+            q1.pop();
+        }
+        else
+        {
+            answer = cnt;
+            break;
+        }
+
+        cnt++;
+    }
+
+    return answer;
+}
