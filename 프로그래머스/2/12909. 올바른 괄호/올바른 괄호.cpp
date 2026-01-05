8cdb1956856ab838463852e977bf2fd294c7cc99
LEE MINGU <56715189+dlalsrn@users.noreply.github.com>
2026-01-05 09:22:17 +0900

    [level 2] Title: 올바른 괄호, Time: 0.58 ms, Memory: 3.79 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <stack>
+using namespace std;
+
+bool solution(string s)
+{
+    stack<char> st;
+
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (s[i] == '(')
+        {
+            st.push(s[i]);
+        }
+        else
+        {
+            if (st.empty())
+            {
+                return false;
+            }
+            else
+            {
+                st.pop();
+            }
+        }
+    }
+
+    if (!st.empty())
+    {
+        return false;
+    }
+
+    return true;
+}
